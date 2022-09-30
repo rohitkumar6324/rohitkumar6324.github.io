84e1250fe55f71ca6557819cca4e4d328822e0de
Rohit Kumar <kumarrohit6324@gmail.com>
2022-09-30 23:41:28 +0530

    fixed navbar

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 let hey = function(){
     document.getElementById("hamburger").classList.toggle("open");
     document.getElementById("navlist").classList.toggle("open");
+    document.getElementById("navbar").classList.toggle("open");
 }
+
+document.getElementById("hamburger").addEventListener("click",hey);
