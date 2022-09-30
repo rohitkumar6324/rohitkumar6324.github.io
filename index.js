2402565a74af73b063d1f15108858b62902b371f
Rohit Kumar <kumarrohit6324@gmail.com>
2022-10-01 00:03:00 +0530

    fixed navbar with proper responsiveness

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-let hey = function(){
+const toggleHam = function(){
     document.getElementById("hamburger").classList.toggle("open");
     document.getElementById("navlist").classList.toggle("open");
     document.getElementById("navbar").classList.toggle("open");
 }
 
-document.getElementById("hamburger").addEventListener("click",hey);
+document.getElementById("hamburger").addEventListener("click",toggleHam);
+
+let x = [...document.getElementsByClassName("navlist-unit")]
+x.forEach(e => e.addEventListener("click",toggleHam))
+
+
